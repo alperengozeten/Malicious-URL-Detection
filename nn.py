eede0c614149b93aa149da11a69755b76345bb52
HP <alperengozeten@gmail.com>
2023-05-12 18:07:28 +0300

    progress on nn

--- nn.py
+++ nn.py
@@ -1,6 +1,6 @@
 import numpy as np
-from activation_functions import ActivationFunction
-from typing import Any, Type
+from activation_functions import ActivationFunction      
+from typing import Any, Type, Tuple, Iterable
 
 class FullyConnectedLayer:
     def __init__(self, in_features, out_features, activation_func : Type[ActivationFunction]) -> None:
@@ -38,6 +38,24 @@ class FullyConnectedLayer:
     def V(self, X):
         return X @ self._W + self._b
     
+    def init_params(self):
+        generator = np.random.default_rng()
+        self._W = generator.uniform(-1, 1, (self._in_features, self._out_features))
+        self._b = generator.uniform(-1, 1, (1, self._out_features))
+    
     def update_params(self, rate, dW, db):
         self._W = self._W - dW * rate
         self._b = self._b - db * rate
+
+    def __repr__(self) -> str:
+        return 'FullyConnectedLayer(in_features=' + str(self._in_features) + ', out_features=' + str(self._out_features) + ', activation:' + str(self._activation_func)
+
+class NeuralNetwork:
+
+    def __init__(self, layers : Iterable[Tuple[int, int, Type[ActivationFunction]]]) -> None:
+        self._layers = [FullyConnectedLayer(in_features, out_features, activation_func) 
+                        for in_features, out_features, activation_func in layers]
+    
+    @property
+    def layers(self):
+        return self._layers
