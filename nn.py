00d17ab487460c7138692823fabbe342aeb902f1
HP <alperengozeten@gmail.com>
2023-05-12 17:33:01 +0300

    fcl

--- nn.py
+++ nn.py
@@ -1,9 +1,8 @@
 import numpy as np
 from activation_functions import ActivationFunction
-from typing import Type
+from typing import Any, Type
 
 class FullyConnectedLayer:
-
     def __init__(self, in_features, out_features, activation_func : Type[ActivationFunction]) -> None:
         self._activation_func = activation_func()
         self._in_features = in_features
@@ -31,3 +30,14 @@ class FullyConnectedLayer:
     @property
     def out_features(self):
         return self._out_features
+    
+    def __call__(self, X):
+        return self.activation(self.V(X))
+    
+    # returns the induced local field at this layer
+    def V(self, X):
+        return X @ self._W + self._b
+    
+    def update_params(self, rate, dW, db):
+        self._W = self._W - dW * rate
+        self._b = self._b - db * rate
