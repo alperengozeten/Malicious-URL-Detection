a841f82a8b2e8aad78cb52d6ea2217f83cbacb22
HP <alperengozeten@gmail.com>
2023-05-18 14:33:04 +0300

    subplot hyperparams

--- nn_test.py
+++ nn_test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from nn import NeuralNetwork
-from activation_functions import Linear, ReLU  
+from itertools import product
 
 df = pd.read_csv("data/url_processed.csv")
 np.random.seed(2023)
@@ -48,13 +48,46 @@ y_valid = y.iloc[train_set_size: train_set_size + valid_set_size]
 X_test = X.iloc[train_set_size + valid_set_size:]
 y_test = y.iloc[train_set_size + valid_set_size:]
 
-best_model = NeuralNetwork(n_neurons=[64, 64, 1])
-history = best_model.fit(X_train, y_train, X_test, y_test, alpha=0.001, batch_size=32, momentum=0.85,epochs=150, patience=5)
+nn_layers_list = [
+    [64, 1],
+    [32, 32, 1],
+    [64, 64, 1],
+]
+nn_alphas = [1e-2, 5e-3, 1e-3, 5e-4]
+nn_momentums = [0.85, 0.95]
+nn_batch_sizes = [32, 64]
 
-h = pd.DataFrame.from_dict(history)[['train_MSE', 'valid_MSE']]
-h = h.set_axis(['Train MSE', 'Test MSE'], axis=1)
-h.plot()
-plt.xlabel('Epochs')
-plt.ylabel('MSE')
-plt.title('MSE vs Epoch')
-plt.show()
+nn_hyperparams = list(product(nn_alphas, nn_momentums, nn_batch_sizes))
+
+def plot_from_history(history, title):
+    h = pd.DataFrame.from_dict(history)[['train_MSE', 'valid_MSE']]
+    h = h.set_axis(['Train MSE', 'Test MSE'], axis=1)
+    h.plot()
+    plt.xlabel('Epochs')
+    plt.ylabel('MSE')
+    plt.title(title)
+    plt.show()
+
+def subplot_mse(hist_list, nn_hyperparams, layers):
+       fig, axs = plt.subplots(4, 4, figsize=(18, 12))
+       for i in range(4):
+              for j in range(4):
+                     axs[i,j].set_title('Train and Validation MSE')
+                     axs[i,j].set_xlabel('Epochs')
+                     axs[i,j].set_ylabel('Validation MSE')
+                     axs[i,j].plot(hist_list[4 * i + j], label='Batch size = 32')
+                     axs[i,j].legend()
+
+nn_scores = []
+for layers in nn_layers_list:
+     hist_list = []
+     for alpha, momentum, batch_size in nn_hyperparams:
+       nn = NeuralNetwork(n_neurons=layers)
+       history = nn.fit(X_train, y_train, X_valid, y_valid, alpha=alpha, batch_size=batch_size, momentum=momentum, epochs=5, patience=5)
+       hist_list.append(history)
+     subplot_mse(hist_list, nn_hyperparams, layers)
+
+best_model = NeuralNetwork(n_neurons=[64, 64, 1])
+history = best_model.fit(X_train, y_train, X_test, y_test, alpha=0.005, batch_size=32, momentum=0.85,epochs=500, patience=5)
+
+plot_from_history(history, 'MSE vs Epoch')
