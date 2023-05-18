532e2db29241a8dfae2a125525e13ea4f316daca
KaanTekTr <kaan.tek@ug.bilkent.edu.tr>
2023-05-18 16:02:55 +0300

    modify plot

--- nn_test.py
+++ nn_test.py
@@ -74,23 +74,26 @@ def subplot_mse(hist_list, nn_hyperparams, layers):
     for i in range(4):
         for j in range(4):
             axs[i,j].set_title('lr: ' + str(nn_hyperparams[4*i+j][0]) + ', mom: ' + str(nn_hyperparams[4*i+j][1]) + ', bs: ' + str(nn_hyperparams[4*i+j][2]), fontdict={'fontsize': 9})
-            axs[i,j].set_xlabel('Epochs')
-            axs[i,j].set_ylabel('Accuracy')
+            axs[i,j].set_xlabel('Epochs', fontdict={'fontsize': 8})
+            axs[i,j].set_ylabel('Accuracy', fontdict={'fontsize': 8})
             axs[i,j].plot(hist_list[4 * i + j]['train_acc'], label='Train Acc')
             axs[i,j].plot(hist_list[4 * i + j]['test_acc'], label='Validation Acc')
             axs[i,j].legend(fontsize=5)
     plt.suptitle('Hyperparameter Tuning For NN With layers: ' + str(layers))
-    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    plt.subplots_adjust(wspace=0.5, hspace=0.7)
     plt.show()
 
 for layers in nn_layers_list:
     hist_list = []
     for alpha, momentum, batch_size in nn_hyperparams:
         nn = NeuralNetwork(n_neurons=layers)
-        history = nn.fit(X_train, y_train, X_valid, y_valid, alpha=alpha, batch_size=batch_size, momentum=momentum, epochs=3, patience=5)
+        history = nn.fit(X_train, y_train, X_valid, y_valid, alpha=alpha, batch_size=batch_size, momentum=momentum, epochs=200, patience=5)
         hist_list.append(history)
     subplot_mse(hist_list, nn_hyperparams, layers)
 
+# train the best model 
+X_train = X.iloc[:train_set_size + valid_set_size]
+y_train = y.iloc[:train_set_size + valid_set_size]
 best_model = NeuralNetwork(n_neurons=[64, 64, 1])
 history = best_model.fit(X_train, y_train, X_test, y_test, alpha=0.005, batch_size=32, momentum=0.85,epochs=500, patience=5)
 
