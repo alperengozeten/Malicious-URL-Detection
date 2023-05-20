75915a2c16720cb57d80886f14940d03931f4591
HP <alperengozeten@gmail.com>
2023-05-20 17:25:12 +0300

    add feat

--- nn_test.py
+++ nn_test.py
@@ -17,7 +17,7 @@ X = df[['use_of_ip', 'url_length', 'subdomain_length', 'tld_length', 'fld_length
        'count%', 'count?', 'count=', 'count+', 'count/', 'count,', 'count!',
        'letters_ratio', 'digit_ratio', 'punc_ratio', 'count_dirs',
        'use_of_shortener', 'first_dir_length',
-       'url_length_q', 'fld_length_q', 'https', 'count-https', 'count-http', 'count-www', 'sus_url']]
+       'url_length_q', 'fld_length_q', 'https', 'count-https', 'count-http', 'count-www', 'sus_url', 'is_php']]
 
 print(X.head())
 
